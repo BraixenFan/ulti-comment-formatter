0a912df0df10e40e9ba4502d58ab718f35c6d5bc
Carlos Plata <carlos.plata282@gmail.com>
2024-04-29 16:42:47 -0600

    formatted the comment and displayed it

--- app.js
+++ app.js
@@ -6,6 +6,14 @@ submitButton.addEventListener("click", startFormatting);
 
 function startFormatting() {
   let comment = commentInput.value;
+  console.log(comment);
 
-  commentOutput.textContent = comment;
+  let date = comment.substr(comment.indexOf("[") + 1, comment.indexOf("]") - 1);
+  let username = comment.substr(comment.indexOf("From") + 5, comment.indexOf(":") - 16);
+  let message = comment.substr(comment.indexOf(":") + 2, comment.lenght);
+  //message.replace(/\r?\n/g, "<br>"); doesn't work
+
+  let formattedComment = "<strong>" + username + "</strong><br>" + date + "<br>\n" + message + "<br><br><br>";
+
+  commentOutput.textContent = formattedComment;
 }

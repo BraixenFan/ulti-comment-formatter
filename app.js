9e44cc5312ccbfd91806ef3b7c5c6ebba92c1c5c
Carlos Plata <carlos.plata282@gmail.com>
2024-04-30 09:43:43 -0600

    added implemantation for multiple comments

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const commentInput = document.getElementById("comment-input");
 const submitButton = document.getElementById("submit-comment");
+const submitMultipleButton = document.getElementById("submit-comment-multiple");
 const commentOutput = document.getElementById("formatted-output");
 
 submitButton.addEventListener("click", startFormatting);
+submitMultipleButton.addEventListener("click", loopFormatting);
 
 function startFormatting() {
   let comment = commentInput.value;
@@ -16,4 +18,23 @@ function startFormatting() {
   let formattedComment = "<strong>" + username + "</strong><br>" + date + "<br>\n" + message + "<br><br><br>";
 
   commentOutput.textContent = formattedComment;
+}
+
+function loopFormatting() {
+  let comments = commentInput.value;
+  comments = comments.split("---");
+  commentOutput.textContent = "";
+
+  console.log(comments)
+
+  comments.forEach((comment) => {
+    let date = comment.substr(comment.indexOf("[") + 1, comment.indexOf("]") - 1);
+    let username = comment.substr(comment.indexOf("From") + 5, comment.indexOf(":") - 16);
+    let message = comment.substr(comment.indexOf(":") + 2, comment.lenght);
+    console.log(date)
+    //message.replace(/\r?\n/g, "<br>"); doesn't work
+
+    let formattedComment = "\n\n<strong>" + username + "</strong><br>" + date + "<br>\n" + message + "<br><br><br>";
+    commentOutput.textContent = commentOutput.textContent + formattedComment;
+  })
 }

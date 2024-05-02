101d0ac98b21eaa02a42e6a41344e0175f4acdc6
Carlos Plata <carlos.plata282@gmail.com>
2024-05-02 09:40:02 -0600

    updated single comment functionality
    Also fixed typo

--- app.js
+++ app.js
@@ -11,9 +11,10 @@ function startFormatting() {
   console.log(comment);
 
   let date = comment.substr(comment.indexOf("[") + 1, comment.indexOf("]") - 1);
-  let username = comment.substr(comment.indexOf("From") + 5, comment.indexOf(":") - 16);
-  let message = comment.substr(comment.indexOf(":") + 2, comment.lenght);
+  let username = comment.substr(comment.indexOf("From") + date.length - 3, comment.indexOf(":") - comment.indexOf("From") - 5);
+  let message = comment.substr(comment.indexOf(":") + 2, comment.length - username.length - date.length);
   //message.replace(/\r?\n/g, "<br>"); doesn't work
+  console.log(date.length);
 
   let formattedComment = "<strong>" + username + "</strong><br>" + date + "<br>\n" + message + "<br><br><br>";
 
@@ -30,7 +31,7 @@ function loopFormatting() {
   comments.forEach((comment) => {
     let date = comment.substr(comment.indexOf("[") + 1, comment.indexOf("]") - 1);
     let username = comment.substr(comment.indexOf("From") + 5, comment.indexOf(":") - 16);
-    let message = comment.substr(comment.indexOf(":") + 2, comment.lenght);
+    let message = comment.substr(comment.indexOf(":") + 2, comment.length);
     console.log(date)
     //message.replace(/\r?\n/g, "<br>"); doesn't work
 
